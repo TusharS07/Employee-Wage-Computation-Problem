1d66d86f954a1d33a5af6a83b838f71ef5e85c86
Salunkhe@07 <salunkhet07@gmail.com>
2022-09-24 21:53:17 +0530

    UC-2

---
+++ EmpDailyWageUC2.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.EmployeeWageComputationday3;
+
+public class EmpDailyWageUC2 extends EmployeeWageComputation{
+    public double calculateEmpDailyWage(int perHourWage, int fullDayHours) {
+        return perHourWage * fullDayHours;
+    }
+
+}
